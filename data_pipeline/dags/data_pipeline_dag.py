9fbe9c914020d98e5251e68f63322397cad579e6
Divyia Venkat <eachampattithiruna.d@northeastern.edu>
2024-11-03 01:42:35 -0500

    adding email alerts for data pipeline

--- data_pipeline/dags/data_pipeline_dag.py
+++ data_pipeline/dags/data_pipeline_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from airflow.operators.email import EmailOperator
 
 from data_pipeline import (
     load_reviews_dataset,
@@ -13,6 +14,7 @@ from data_pipeline import (
     apply_vader_sentiment,
     apply_textblob_sentiment,
     apply_majority_vote,
+    failure_email,
 )
 
 # Define default arguments for the DAG
@@ -35,52 +37,72 @@ with DAG(
 
     load_reviews_task = PythonOperator(
         task_id='load_reviews_dataset',
-        python_callable=load_reviews_dataset
+        python_callable=load_reviews_dataset,
+        on_failure_callback=failure_email
     )
 
     load_metadata_task = PythonOperator(
         task_id='load_metadata_dataset',
-        python_callable=load_metadata_dataset
+        python_callable=load_metadata_dataset,
+        on_failure_callback=failure_email
     )
 
     merge_data_task = PythonOperator(
         task_id='merge_data',
-        python_callable=merge_data
+        python_callable=merge_data,
+        on_failure_callback=failure_email
     )
 
     remove_html_tags_task = PythonOperator(
         task_id='remove_html_tags',
-        python_callable=remove_html_tags
+        python_callable=remove_html_tags,
+        on_failure_callback=failure_email
     )
 
     filter_urls_paths_task = PythonOperator(
         task_id='filter_urls_paths',
-        python_callable=filter_urls_paths
+        python_callable=filter_urls_paths,
+        on_failure_callback=failure_email
     )
 
     remove_non_string_reviews_task = PythonOperator(
         task_id='remove_non_string_reviews',
-        python_callable=remove_non_string_reviews
+        python_callable=remove_non_string_reviews,
+        on_failure_callback=failure_email
     )
 
     heuristic_sentiment_task = PythonOperator(
         task_id='heuristic_sentiment_analysis',
-        python_callable=apply_heuristic_sentiment
+        python_callable=apply_heuristic_sentiment,
+        on_failure_callback=failure_email
     )
 
     vader_sentiment_task = PythonOperator(
         task_id='vader_sentiment_analysis',
-        python_callable=apply_vader_sentiment
+        python_callable=apply_vader_sentiment,
+        on_failure_callback=failure_email
     )
 
     textblob_sentiment_task = PythonOperator(
         task_id='textblob_sentiment_analysis',
-        python_callable=apply_textblob_sentiment
+        python_callable=apply_textblob_sentiment,
+        on_failure_callback=failure_email
     )
 
     majority_vote_task = PythonOperator(
         task_id='apply_majority_vote',
-        python_callable=apply_majority_vote
+        python_callable=apply_majority_vote,
+        on_failure_callback=failure_email
+    )
+
+    # Email operator for sending success notification
+    send_email_success = EmailOperator(
+        task_id='send_email_success',
+        to='testmlops@yopmail.com',
+        subject='Data Pipeline Success: Sentiment Analysis Data Pipeline Completed',
+        html_content="<p>The sentiment analysis data extraction and labeling pipeline has completed successfully.</p>",
+        trigger_rule='all_success',
+        dag=dag
     )
 
     # Set up dependencies
@@ -88,4 +110,4 @@ with DAG(
     remove_non_string_reviews_task >> [
         heuristic_sentiment_task, vader_sentiment_task, textblob_sentiment_task]
     [heuristic_sentiment_task, vader_sentiment_task,
-        textblob_sentiment_task] >> majority_vote_task
+        textblob_sentiment_task] >> majority_vote_task >> send_email_success
